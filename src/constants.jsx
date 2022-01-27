2d9009f7b6a63cfe7a92c5f4f57980f13e92f273
gaikar1990@gmail.com <gaikar1990@gmail.com>
2022-01-27 18:58:27 +0530

    Folder Re-Structure

---
+++ src/constants.jsx
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x56987e0a3685F022607D20e57D46683B394E982d'
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  }
+}
+
+export { CONTRACT_ADDRESS, transformCharacterData }
